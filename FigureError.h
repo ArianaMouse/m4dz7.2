13545a42c96305206f0d88e81bb2f6fbb149fee1
Ariana Makarova <ariana.arfwkul@gmail.com>
2025-10-25 14:16:57 +0300

    add figureError.h

---
+++ FigureError.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream> 
+#include <exception>
+#include <stdexcept>
+
+class FigureError : public std::domain_error {
+public:
+    explicit FigureError(const std::string& message) : std::domain_error(message) {}
+};
